6ff22568319e12c0211d3ed479d6a7a3668c37d4
akzmffls <d7703_14@d7703-14ui-imac.local>
2018-03-08 11:35:54 +0900

    Initial Commit

---
+++ myFirst-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myFirst-2
+//
+//  Created by D7703_14 on 2018. 3. 8..
+//  Copyright © 2018년 Personal Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
